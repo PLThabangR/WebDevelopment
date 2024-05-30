8ed8c2bf30e8b46e40ad11886377c0d9a3c05692
PLThabangR <rakgoropthabang@gmail.com>
2024-05-30 09:21:06 +0200

    Math object

---
+++ javascript/Objects.js
@@ -0,0 +1,39 @@
+//creating an object with properties and their values
+var assistantManager = {
+    rangeTilesPerTurn: 3,
+    socialSkills: 30,
+    streetSmarts: 30,
+    health: 40,
+    specialAbility: "young and ambitious",
+    greeting: "Let's make some money"
+}
+
+assistantManager.greeting="Hello"
+
+console.log(assistantManager.greeting)
+
+var table = {
+    legs: 3,
+    color: "brown",
+    priceUSD: 100,
+}
+
+var house = {
+    rooms: 3,
+    color: "brown",
+    priceUSD: 10000,
+}
+
+var fruits =[]
+fruits.push("apple");
+fruits.push("Kiwi")
+
+console.log(fruits)
+
+fruits.reverse()
+
+console.log(fruits)
+
+//MAth method built in function
+
+console.log(Math.round(Math.random()*100,2))
--- javascript/letterFinder.js
+++ javascript/letterFinder.js
@@ -2,7 +2,7 @@
 
 function letterFinder(word,match){
 
-    for(i=0;word.length;i++){
+    for(i=0;x<word.length;i++){
         if(word[i] == match){
             console.log('Found the', match, 'at', i)
         }else{
